d96f3b9302fefb3e52a448fa5ffe9beb1c00b60b
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 08:22:38 +0100

    chore: update plugin categories (#9)
    * chore: update kestraVersion to 1.2.3
    
    * chore: update plugin categories
    
    ---------
    
    Co-authored-by: François Delbrayelle <fdelbrayelle@kestra.io>

--- src/main/java/io/kestra/plugin/zenduty/package-info.java
+++ src/main/java/io/kestra/plugin/zenduty/package-info.java
@@ -1,6 +1,6 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for Zenduty notifications.",
-    categories = PluginSubGroup.PluginCategory.ALERTING
+    categories = PluginSubGroup.PluginCategory.BUSINESS
 )
 package io.kestra.plugin.zenduty;
 
